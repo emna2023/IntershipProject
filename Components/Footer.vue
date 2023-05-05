62d46ab311b541a8d9f89672b6efaacf63d30437
Emna Khemakhem <khemakhememna@cedeg.be>
2023-05-05 13:56:01 +0200

    squelettepush

---
+++ Components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+<footer>
+
+<h1>PowerUp</h1>
+<h2>Le fan site par officiel du tout de chainsaw man</h2>
+
+</footer>
+</template>
---
+++ Components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <header>
+    
+    <h1>PowerUp</h1>
+    <h2>Le fan site par officiel du tout de chainsaw man</h2>
+    
+    </header>
+    </template>
